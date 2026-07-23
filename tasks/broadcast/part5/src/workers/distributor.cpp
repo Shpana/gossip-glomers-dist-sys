06e2f36f4df42597fd009a7f972aff2b4d3a07f9
Roman Kozlov <54642655+Shpana@users.noreply.github.com>
2026-07-23 15:32:51 +0000

    feat broadcast/part5: adapt to new maelstrom lib

--- tasks/broadcast/part5/src/workers/distributor.cpp
+++ tasks/broadcast/part5/src/workers/distributor.cpp
@@ -1,71 +1,70 @@
 #include "workers/distributor.hpp"
 
+#include "handlers/broadcast.hpp"
+
 #include <yaclib/async/make.hpp>
 #include <yaclib/async/when_all.hpp>
 
-#include "network/messages.hpp"
-#include "network/network.hpp"
+#include <maelstrom/network/messages.hpp>
+#include <maelstrom/network/network.hpp>
 
-#include "handlers/broadcast.hpp"
+namespace tasks::broadcast::part5 {
 
-namespace ds::broadcast {
-  yaclib::Future<>
-  DistributorWorker::process(maelstrom::Network::Session&& session) {
-    LOG_INFO() << "distributor called!\n";
+yaclib::Future<>
+DistributorWorker::Process(maelstrom::Network::Session session) {
+  auto &info = GetState().info;
+  auto &storage = GetState().storage;
 
-    auto& info = state_->info;
-    auto& storage = state_->storage;
+  using namespace std::chrono_literals;
+  constexpr maelstrom::Network::Clock::duration kTimeout{300ms};
 
-    if (!info.ready.load()) {
-      return yaclib::MakeFuture();
-    }
-
-    std::vector<std::string> all_node_ids;
-    {
-      std::lock_guard guard{info.mtx};
-      all_node_ids = info.all_node_ids;
-    }
-
-    std::vector<yaclib::Future<>> futs;
-    futs.reserve(all_node_ids.size());
-
-    using namespace std::chrono_literals;
-    constexpr maelstrom::Network::Clock::duration timeout{300ms};
-
-    {
-      std::lock_guard guard{storage.mtx};
-
-      for (auto& node_id: all_node_ids) {
-        if (!storage.maybe_unknown.contains(node_id)) [[unlikely]] {
-          storage.maybe_unknown[node_id] = 0;
-        }
-
-        if (storage.maybe_unknown[node_id] == storage.nums.size()) {
-          continue;
-        }
-
-        auto body = nlohmann::json({});
-        body["messages"] = std::vector<std::uint64_t>{
-            storage.nums.begin() + storage.maybe_unknown[node_id],
-            storage.nums.end()};
-
-        futs.push_back(
-            session
-                .call<BroadcastBulkHandler>(node_id, std::move(body), timeout)
-                .ThenInline([&storage, node_id, prev_end = storage.nums.size()](
-                                yaclib::Result<maelstrom::Response> result) {
-                  if (result) {
-                    std::lock_guard guard{storage.mtx};
-                    storage.maybe_unknown[node_id] = prev_end;
-                  }
-                }));
-      }
-    }
-
-    if (futs.empty()) {
-      return yaclib::MakeFuture();
-    }
-
-    return yaclib::WhenAll(futs.begin(), futs.end()).ThenInline([](auto&&) {});
+  if (!info.ready.load()) {
+    return yaclib::MakeFuture();
   }
-}// namespace ds::broadcast
+
+  std::vector<std::string> all_node_ids;
+  {
+    std::lock_guard guard{info.mtx};
+    all_node_ids = info.all_node_ids;
+  }
+
+  std::vector<yaclib::Future<>> futs;
+  futs.reserve(all_node_ids.size());
+
+  {
+    std::lock_guard guard{storage.mtx};
+
+    for (auto &node_id : all_node_ids) {
+      if (!storage.maybe_unknown.contains(node_id)) [[unlikely]] {
+        storage.maybe_unknown[node_id] = 0;
+      }
+
+      if (storage.maybe_unknown[node_id] == storage.nums.size()) {
+        continue;
+      }
+
+      auto body = nlohmann::json({});
+      body["messages"] = std::vector<std::uint64_t>{
+        storage.nums.begin() + storage.maybe_unknown[node_id],
+        storage.nums.end()};
+
+      futs.push_back(
+        session.Call<BroadcastBulkHandler>(node_id, std::move(body), kTimeout)
+          .ThenInline([&storage, node_id, prev_end = storage.nums.size()](
+                        yaclib::Result<maelstrom::Response> &&result) {
+            if (result) {
+              std::lock_guard guard{storage.mtx};
+              storage.maybe_unknown[node_id] = prev_end;
+            }
+          }));
+    }
+  }
+
+  if (futs.empty()) {
+    return {};
+  }
+
+  return yaclib::WhenAll(futs.begin(), futs.end());
+}
+
+} // namespace tasks::broadcast::part5
