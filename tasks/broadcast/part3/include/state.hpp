a679ce03d0f14df9edfd12511aa265a82f0c6c04
Roman Kozlov <54642655+Shpana@users.noreply.github.com>
2026-07-23 14:53:41 +0000

    feat broadcast/part3: adapt to new maelstrom lib

--- tasks/broadcast/part3/include/state.hpp
+++ tasks/broadcast/part3/include/state.hpp
@@ -7,11 +7,13 @@
 #include <unordered_map>
 #include <vector>
 
-namespace ds::broadcast {
-  struct State {
-    std::mutex mtx_{};
-    std::vector<std::uint64_t> nums;// Guarded by mtx_;
-    std::optional<std::unordered_map<std::string, std::vector<std::string>>>
-        topology{std::nullopt};
-  };
-}// namespace ds::broadcast
+namespace tasks::broadcast::part3 {
+
+struct State {
+  std::mutex mtx{};
+  std::vector<std::uint64_t> nums; // Guarded by mtx
+  std::optional<std::unordered_map<std::string, std::vector<std::string>>>
+    topology{std::nullopt};
+};
+
+} // namespace tasks::broadcast::part3
